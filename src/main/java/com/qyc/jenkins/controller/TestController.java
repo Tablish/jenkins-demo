6df6444ad857b47613b636339361191ffa05c339
Tablish <qyc772528883@163.com>
2021-11-19 00:21:39 +0800

    服了

--- src/main/java/com/qyc/jenkins/controller/TestController.java
+++ src/main/java/com/qyc/jenkins/controller/TestController.java
@@ -2,7 +2,6 @@ package com.qyc.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  * @Description TODO
  * @Author qianyongchao
