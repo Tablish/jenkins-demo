c2939337a5e8bd86fb1e799b9a0a33ffbae11948
qianyongchao <qyc772528883@163.com>
2021-11-18 23:09:02 +0800

    first commit

---
+++ src/main/java/com/qyc/jenkins/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.qyc.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author qianyongchao
+ * @CreateTime 2021-11-18 23:01:00
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/qyc/jenkins/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.qyc.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author qianyongchao
+ * @CreateTime 2021-11-18 22:59:00
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        System.out.println("hello world");
+        return "hello world";
+    }
+}
