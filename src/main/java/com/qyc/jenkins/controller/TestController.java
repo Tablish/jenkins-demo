da38b104379140ee949312d8234e64d361d62d31
Tablish <qyc772528883@163.com>
2021-11-19 00:01:59 +0800

    test

--- src/main/java/com/qyc/jenkins/controller/TestController.java
+++ src/main/java/com/qyc/jenkins/controller/TestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @GetMapping("test")
     public String test() {
         System.out.println("hello world");
